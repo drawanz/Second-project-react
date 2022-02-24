0613cf9145114641b7a6610449ddd6f5590d7078
Leonidas <leonidas.drawanz@gmail.com>
2022-02-24 15:24:15 -0300

    passando os itens de componentes para itens do form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,71 @@
 import React, { Component } from 'react';
-import NameInput from './Name';
-import DescriptionInput from './Description';
-import Atributo from './Atributo';
-import Image from './Image';
-import Raridade from './Raridade';
-import SuperTrunfo from './SuperTrunfo';
-import Button from './Button';
 
 export default class Form extends Component {
   render() {
     return (
       <div>
         <form>
-          <NameInput />
-          <DescriptionInput />
-          <Atributo />
-          <Image />
-          <Raridade />
-          <SuperTrunfo />
-          <Button />
+          <label htmlFor="nameInput">
+            Nome da carta:
+            <input
+              id="nameInput"
+              type="text"
+              data-testid="name-input"
+              placeholder="Placeholder"
+            />
+          </label>
+
+          <label htmlFor="descriptionInput">
+            Descrição da carta:
+            <textarea
+              id="descriptionInput"
+              type="text"
+              data-testid="description-input"
+            />
+          </label>
+
+          <label htmlFor="atributoInput">
+            Atributo 1 da carta:
+            <input id="atributoInput" type="number" data-testid="attr1-input" />
+          </label>
+
+          <label htmlFor="atributoInput">
+            Atributo 2 da carta:
+            <input id="atributoInput" type="number" data-testid="attr2-input" />
+          </label>
+
+          <label htmlFor="atributoInput">
+            Atributo 3 da carta:
+            <input id="atributoInput" type="number" data-testid="attr3-input" />
+          </label>
+
+          <label htmlFor="imageInput">
+            URL da imagem:
+            <input id="imageInput" type="text" data-testid="image-input" />
+          </label>
+
+          <label htmlFor="rareInput">
+            Raridade da carta:
+            <select id="rareInput" type="text" data-testid="rare-input">
+              <option value="normal">normal</option>
+              <option value="raro">raro</option>
+              <option value="muito raro">muito raro</option>
+            </select>
+          </label>
+
+          <label htmlFor="SuperTrunfo">
+            <input
+              id="SuperTrunfo"
+              type="checkbox"
+              data-testid="trunfo-input"
+            />
+            Super Trybe Trunfo
+          </label>
+
+          <button type="button" data-testid="save-button">
+            Salvar
+          </button>
+
         </form>
       </div>
     );
